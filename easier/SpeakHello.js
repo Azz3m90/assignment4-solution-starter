5abe049f864bdcbd03544a4fe8ec1303b6a5e264
azzam <azzamazezali@gmail.com>
2021-08-05 20:45:33 +0300

    sayHello

--- easier/SpeakHello.js
+++ easier/SpeakHello.js
@@ -23,7 +23,4 @@ window.helloSpeaker = helloSpeaker;
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 // xxxx.xxxx = helloSpeaker;
 })(window);
-<<<<<<< HEAD
 //
-=======
->>>>>>> 005a645718515975e6122bfbfe960f2cafe63be2
